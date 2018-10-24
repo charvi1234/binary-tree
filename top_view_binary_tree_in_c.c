344c6359e5564dc143e42a7fe0e35d3e8b4da454
Charvi bansal <32154571+charvi1234@users.noreply.github.com>
2018-10-24 19:55:29 +0530

    Update top_view_binary_tree_in_c.c

--- top_view_binary_tree_in_c.c
+++ top_view_binary_tree_in_c.c
@@ -1,3 +1,24 @@
+/*
+       1
+    /     \
+   2       3
+  /  \    / \
+ 4    5  6   7
+Top view of the above binary tree is
+4 2 1 3 7
+
+        1
+      /   \
+    2       3
+      \   
+        4  
+          \
+            5
+             \
+               6
+Top view of the above binary tree is
+2 1 3 6
+*/
 #include<stdio.h>
 #include<stdlib.h>
 typedef struct node
