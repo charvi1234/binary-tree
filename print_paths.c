0686c01e7ea86300666bf9146f8cfc63e548c952
Charvi bansal <32154571+charvi1234@users.noreply.github.com>
2018-11-01 19:02:45 +0530

    Update print_paths.c

--- print_paths.c
+++ print_paths.c
@@ -1,3 +1,19 @@
+/*
+      6
+     /    \
+    3      5
+  /   \     \
+ 2     5     4
+     /   \
+    7     4
+
+There are 4 leaves, hence 4 root to leaf paths -
+  6->3->2              
+  6->3->5->7
+  6->3->5->4
+  6->5>4
+  
+*/
 #include<stdio.h>
 #include<stdlib.h>
 typedef struct node
